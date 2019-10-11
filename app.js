33349c557fafa06a0f88d19db61ef01908b5c39d
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-10-10 19:43:51 -0500

    added mongo db connection in app

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ var app = express();
 
 var config = require('./config.dev.js');
 
+var mongoose = require('mongoose');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,12 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,6 +45,10 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+mongoose.connect(config.mongodb, {
+  userNewUrlParser: true
+});
 
 console.log("connected to ");
 console.log(config);
