7b134f92035b46114e31a3fb98c2cc3a41570f6a
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-10-10 19:57:12 -0500

    added routing file

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var config = require('./config.dev.js');
 
 var mongoose = require('mongoose');
 
+var apiUsersRouter = require('./routes/api/users');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/user', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
